4d0c9638e79f99d6c555cc8d735460640295c348
zach <ZCroninDev@users.noreply.github.com>
2021-05-28 20:10:51 -0400

    Animates circle timer and adds numeric countdown

--- app.js
+++ app.js
@@ -32,7 +32,22 @@ const app = () =>  {
             video.pause();
             play.src = './svg/play.svg';
         }  
-        }
     };
+
+    //circle animation
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+    
+
+        //animate the circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+        //animate the display 
+        timeDisplay.textContent = `${minutes}: ${seconds}`;
+    };
+};
 
 app();
